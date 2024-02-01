3560569a25360c8c96e3ad198638e694e0e82aa8
Seth Hobson <sethlawrencehobson@gmail.com>
2024-02-01 15:31:34 -0500

    added formdata handling on handle submit

--- src/PhotoForm.js
+++ src/PhotoForm.js
@@ -10,26 +10,35 @@ const BASE_API_URL = "http://localhost:5000";
  */
 
 function PhotoForm() {
-  const [formData, setFormData] = useState();
+  const [fileData, setFileData] = useState();
 
-  console.log("Photo Form, file:", formData?.name);
+  console.log("Photo Form, file:", fileData?.name);
+
 
   async function handleSubmit(evt) {
+    debugger;
     evt.preventDefault();
+    const formData = new FormData();
+    formData.filepath = fileData
+    formData.object_name = fileData.name
+    // formData.append('filepath', fileData);
+    // formData.append('object_name', fileData.name);
+
+    console.log('FORMDATA',formData)
+
     if(formData) {
       await fetch(`${BASE_API_URL}/api/photos`, {method: "POST",
-        form: ({
-          "filepath": formData,
-          "object_name": formData.name}),
+        body: formData,
         headers: { 'content-type': 'multipart/form-data' }}
       );
     }
   }
 
   function handleChange(evt) {
-    setFormData(evt.target.files[0]);
+    setFileData(evt.target.files[0]);
+    console.log('FILEDATA',fileData)
+    console.log('made it to handle change')
   }
-
 
 
   return(
